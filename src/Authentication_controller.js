15b27dfaf0f679d740f37084f8552e0f6eec32fc
Matheus <theusmoraes@gmail.com>
2020-09-06 17:44:55 -0300

    Utilizado bycript para encriptografar senhas

--- src/Authentication_controller.js
+++ src/Authentication_controller.js
@@ -1,19 +1,40 @@
 const User = require('./userModel')
+const bycrpt = require("bcrypt")
 module.exports ={
     async login(req,res){
         const {user, password} = req.body
         console.log(user + " " + password)
-        res.send('Senha:' + req.password)
+        let mongoUser = await User.findOne({email: user})
+        if (mongoUser){
+            if(bycrpt.compareSync(password, mongoUser.password)){
+                console.log("SENHA ACEITA")
+                res.send("LOGADO");
+            }else{
+                console.log("SENHA Errada")
+
+                res.send("Email ou senha Incorreto")
+            }
+
+        }else{
+            console.log("Email Errado")
+
+            res.send("Email ou senha Incorreto")
+        }
+
     },
+
     async register(req, res){
         const {email, password, name} = req.body
-        console.log ("Email:"+ email + " Senha:" + password + " Name:" + name )
         let user = await User.findOne({email})
-        try {
-            user = await User.create({name: name, email: email, password:password })
-        }catch(e){
-            console.log(e)
-
+        if (!user){
+            bycrptPassword = bycrpt.hashSync(password, 10)
+            try {
+                user = await User.create({name: name, email: email, password:bycrptPassword})
+            }catch(e){
+                console.log(e)
+            }
+        }else{
+            console.log("Email ja cadastrado")
         }
         res.send("Registro realizado")
     }
